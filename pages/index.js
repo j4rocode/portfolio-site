aa1448f90bf84f9aea6b126a1574a75a58bfdde1
j4rocode <piotr.jarosz10@gmail.com>
2022-09-13 08:22:12 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,12 +11,12 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
+      
         <h1 className={styles.title}>
          STRONA W BUDOWIE
         </h1>
 
-       </main>
+       
 
       <footer className={styles.footer}>
         <a
